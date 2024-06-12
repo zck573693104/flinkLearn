ccd17c00570cee369276e0d9c46d1eb71afa638a
zck <573693104@qq.com>
2024-06-12 14:47:04 +0800

    CODE13

---
+++ src/main/java/com/bigdata/leetcode/CODE13.java
@@ -0,0 +1,55 @@
+package com.bigdata.leetcode;
+
+public class CODE13 {
+    public static void main(String[] args) {
+        String nums1 = "III";
+        System.out.println(romanToInt(nums1));
+    }
+
+    public static int romanToInt(String s) {
+        s = s.replace("IV", "a");
+        s = s.replace("IX", "b");
+        s = s.replace("XL", "c");
+        s = s.replace("XC", "d");
+        s = s.replace("CD", "e");
+        s = s.replace("CM", "f");
+
+        int result = 0;
+        for (int i = 0; i < s.length(); i++) {
+            result += which(s.charAt(i));
+        }
+        return result;
+    }
+
+    public static int which(char ch) {
+        switch (ch) {
+            case 'I':
+                return 1;
+            case 'V':
+                return 5;
+            case 'X':
+                return 10;
+            case 'L':
+                return 50;
+            case 'C':
+                return 100;
+            case 'D':
+                return 500;
+            case 'M':
+                return 1000;
+            case 'a':
+                return 4;
+            case 'b':
+                return 9;
+            case 'c':
+                return 40;
+            case 'd':
+                return 90;
+            case 'e':
+                return 400;
+            case 'f':
+                return 900;
+        }
+        return 0;
+    }
+}
